b64905a5251f3ffe69939694e06b2176b18e9246
M <miguel.neves3@etec.sp.gov.br>
2025-06-11 21:21:01 -0300

    Update scripts.js
    modal de tela cheia adicionado

--- scripts.js
+++ scripts.js
@@ -4,3 +4,25 @@ const navMenu = document.getElementById("nav-menu");
 toggleBtn.addEventListener("click", () => {
   navMenu.classList.toggle("active");
 });
+
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modalImg");
+const closeModal = document.getElementById("closeModal");
+
+document.querySelectorAll(".gallery img").forEach(img => {
+  img.addEventListener("click", () => {
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    modalImg.alt = img.alt;
+  });
+});
+
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", e => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
